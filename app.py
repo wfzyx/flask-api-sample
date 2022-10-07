7e49ec8bf21e3716f7a1f5bf0c02ac65f79a43e7
Live System User <victor.paisa@gmail.com>
2022-10-07 08:56:54 -0400

    Adds proper data parsing according to accetance criteria

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import requests
 from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
-ein_index = {}
+# ein_index = {}
 name_index = {}
 
 
-def get_index_by_company_name(search_argument):
+def function_get_index_by_company_name(search_argument):
     return {
         company_name: url
         for company_name, url in name_index.items()
@@ -18,14 +18,30 @@ def get_index_by_company_name(search_argument):
     }
 
 
+@app.route("/api/fetch")
+def endpoint_get_listings_by_index_file():
+    response = requests.get(request.args.get("index_file"))
+    data = json.loads(response.text)
+    plan_name_list = []
+    network_file_list = []
+    for reporting_structure in data["reporting_structure"]:
+        for plan in reporting_structure["reporting_plans"]:
+            plan_name_list.append(plan["plan_name"])
+        for network_file in reporting_structure["in_network_files"]:
+            network_file_list.append(network_file["location"])
+    return jsonify(
+        {"plan_name_list": plan_name_list, "network_file_list": network_file_list}
+    )
+
+
 @app.route("/api/search/byname/<search_argument>")
-def api_get_company_name_list(search_argument):
-    return jsonify(get_index_by_company_name(search_argument))
+def endpoint_get_company_name_list(search_argument):
+    return jsonify(function_get_index_by_company_name(search_argument))
 
 
 @app.route("/result")
 def view_result_by_company_name():
-    result_set = get_index_by_company_name(request.args.get("search_argument"))
+    result_set = function_get_index_by_company_name(request.args.get("search_argument"))
     return render_template(
         "result.html",
         result_set=result_set,
@@ -33,11 +49,11 @@ def view_result_by_company_name():
 
 
 @app.route("/")
-def index():
+def view_index():
     return render_template("index.html")
 
 
-def build_name_index():
+def function_build_name_index():
     api_url = "https://transparency-in-coverage.uhc.com/api/v1/uhc/blobs/"
     response = requests.get(api_url)
     data = json.loads(response.text)
@@ -68,5 +84,5 @@ def build_name_index():
 
 
 if __name__ == "__main__":
-    build_name_index()
+    function_build_name_index()
     app.run(debug=True)
