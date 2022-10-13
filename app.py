57131d29fcf598969370fd63c3a8ce24048a0fdc
Victor Panisa <victor.panisa@gmail.com>
2022-10-13 00:08:25 -0300

    Implements sqlite3 backend and background multiproccessing [WIP]

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
+#!/usr/bin/env python
 import json
+from multiprocessing import Process
 
 import requests
 from flask import Flask, jsonify, render_template, request
 
+from repository import Repository
+
 app = Flask(__name__)
-# ein_index = {}
-name_index = {}
 
 
 def function_get_index_by_company_name(search_argument):
-    return {
-        company_name: url
-        for company_name, url in name_index.items()
-        if search_argument.lower() in company_name.lower()
-    }
+    return Repository().get_data_by_name(search_argument)
 
 
 @app.route("/api/fetch")
@@ -51,36 +49,38 @@ def view_index():
     return render_template("index.html")
 
 
-def function_build_name_index():
+def background_db_tasks():
+    build_name_index()
+    build_ein_index()
+
+
+def build_name_index():
     api_url = "https://transparency-in-coverage.uhc.com/api/v1/uhc/blobs/"
     response = requests.get(api_url)
     data = json.loads(response.text)
+    sql_data = []
     for item in data["blobs"]:
         if item["name"].endswith("index.json"):
-            formatted_name = (
-                item["downloadUrl"].split("_")[-2].replace("-", " ").strip()
-            )
-            name_index[formatted_name] = item["downloadUrl"]
+            download_url = item["downloadUrl"]
+            clean_name = download_url.split("_")[-2].replace("-", " ").strip()
+            sql_tuple = (download_url, clean_name)
+            sql_data.append(sql_tuple)
+    Repository().insert_bulk_with_name(sql_data)
 
 
-# This is very slow right now, don't use it
-# def build_ein_index():
-#     api_url = "https://transparency-in-coverage.uhc.com/api/v1/uhc/blobs/"
-#     response = requests.get(api_url)
-#     data = json.loads(response.text)
-#     async_requests = [
-#         grequests.get(item["downloadUrl"])
-#         for item in data["blobs"]
-#         if item["name"].endswith("index.json")
-#     ]
-#     for ein_response in grequests.imap(async_requests, size=100):
-#         ein_data = json.loads(ein_response.text)
-#         clean_name = ein_response.url.split("_")[-2].replace("-", " ").strip()
-#         ein_index[
-#             ein_data["reporting_structure"][0]["reporting_plans"][0]["plan_id"]
-#         ] = clean_name
+def build_ein_index():
+    data = Repository().get_rows_without_ein()
+    for ein_response in [requests.get(item[0]) for item in data]:
+        ein_data = json.loads(ein_response.text)
+        json_uri = ein_response.url
+        for struct in ein_data["reporting_structure"]:
+            for plan in struct["reporting_plans"]:
+                ein = plan["plan_id"]
+                Repository().update_ein(json_uri, ein)
 
 
 if __name__ == "__main__":
-    function_build_name_index()
+    Repository().migrate()
+    Process(target=background_db_tasks).start()
+    print("Missing still:", len(Repository().get_rows_without_ein()))
     app.run(debug=True)
